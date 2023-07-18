e3e3cce25dd1d8a184113a01f3c87f2713d514e4
youssef <yberrakouan.lol@gmail.com>
2023-07-18 07:08:02 -0700

    a

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,8 +4,8 @@
  * main - Entry point
  *
  * Description: computes the sum of all
- * 		the multiples of 3 or 5
- * 		below 1024 (excluded)
+ *		the multiples of 3 or 5
+ *		below 1024 (excluded)
  *
  * return: Always 0 (Success)
 */
