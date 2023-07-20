9964540902d2bb9a7f8dd217587998166e294920
youssef <yberrakouan.lol@gmail.com>
2023-07-20 05:21:29 -0700

    a

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  *		left in the year, taking leap years
- * @month: day of month
+ * @month: month in number format
  * @day: day of month
  * @year: year
 */
