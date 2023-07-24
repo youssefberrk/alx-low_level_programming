52d71ad4ff3d7c0f67e34d00da7f633f16cb44a0
youssef <yberrakouan.lol@gmail.com>
2023-07-24 05:43:15 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,40 +2,24 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
+ * @s: string input parameter
  *
- * Return: the int converted from the string
+ * Return: converted integer from string
  */
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	unsigned int num = 0;
+	int sign = 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
+	return (num * sign);
 }
