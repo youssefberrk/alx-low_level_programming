9bc5c05d3aa838cbaea460770edc6a6554261c77
youssef <yberrakouan.lol@gmail.com>
2023-07-24 05:28:31 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,8 +13,8 @@ void puts2(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
-int _atoi(char *s);
 void print_array(int *a, int n);
+int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 
 #endif
