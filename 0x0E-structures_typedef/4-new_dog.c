41c92031f8ad0b881858e18492c4981a52c1ffe0
youssefberrk <yberrakouan.lol@gmail.com>
2023-08-15 05:40:36 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,9 +53,4 @@ int _strlen(char *s)
  * new_dog -  new dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (Success), NULL otherwise
- */
-dog_t *new_dog(char *name, float age, char *owner)
-
+ * @owner: owne
