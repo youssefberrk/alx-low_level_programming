abedea299d63b3f3f863ae8aac280a94f5696252
youssefberrk <yberrakouan.lol@gmail.com>
2023-08-25 05:11:16 -0700

    GG

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
-section .data
-    format db 'Hello, Holberton', 0
+section 	.data
+    format db 'Hello, Holberton', 0xa, 0
 
-section .text
-    global main
-    extern printf
+section 	.text
+    global 	main
+    extern 	printf
 
 main:
-    mov edi, format
-    xor eax, eax
-    call printf
-    mov eax, 0
+    mov 	edi, format
+    xor 	eax, eax
+    call 	printf
+    mov eax, 0xa, 0
     ret
