40db387d82348186505b57889f63cd498774349e
youssefberrk <yberrakouan.lol@gmail.com>
2023-09-05 06:00:23 -0700

    EZ

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,12 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
-
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * @file: The name of the file where the buffer stores characters for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -52,10 +49,10 @@ void close_file(int fd)
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the argument count is incorrect, exit with code 97.
+ * If the source file does not exist or cannot be read, exit with code 98.
+ * If the destination file cannot be created or written to, exit with code 99.
+ * If either the source or destination file cannot be closed, exit with code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -87,7 +84,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
