7e4da9041041ac3796bc8725542714acd184f0c2
youssefberrk <yberrakouan.lol@gmail.com>
2023-09-23 06:01:03 -0700

    EZ

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *allocate_buffer(char *destination);
-void close_descriptor(int fd);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * allocate_buffer - Allocates 1024 bytes of memory for a buffer.
- * @destination: Name of the destination file where characters are stored.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: Pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
-char *allocate_buffer(char *destination)
+char *create_buffer(char *file)
 {
 	char *buffer;
 
@@ -20,7 +20,7 @@ char *allocate_buffer(char *destination)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Unable to allocate memory for %s\n", destination);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -28,76 +28,77 @@ char *allocate_buffer(char *destination)
 }
 
 /**
- * close_descriptor - Close a file descriptor.
- * @fd: File descriptor to be closed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-void close_descriptor(int fd)
+void close_file(int fd)
 {
-	int result;
+	int c;
 
-	result = close(fd);
+	c = close(fd);
 
-	if (result == -1)
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Unable to close file descriptor %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - Copy contents of a source file to a destination file.
- * @argc: Number of arguments supplied to the program.
- * @argv: Array of pointers to the arguments.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If argument count is incorrect, exit with code 97.
- * If source file doesn't exist or cannot be read, exit with code 98.
- * If destination file cannot be created or written to, exit with code 99.
- * If either source or destination file cannot be closed, exit with code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int source, destination, read_bytes, written_bytes;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: my_cp source_file destination_file\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = allocate_buffer(argv[2]);
-	source = open(argv[1], O_RDONLY);
-	read_bytes = read(source, buffer, 1024);
-	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (source == -1 || read_bytes == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Unable to read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		written_bytes = write(destination, buffer, read_bytes);
-		if (destination == -1 || written_bytes == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Unable to write to file %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		read_bytes = read(source, buffer, 1024);
-		destination = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (read_bytes > 0);
+	} while (r > 0);
 
 	free(buffer);
-	close_descriptor(source);
-	close_descriptor(destination);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
+
